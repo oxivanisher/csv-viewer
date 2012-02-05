19fd947df667a1bd3f4f8f62e3b863a9073a0b30
Marc Urben <aegnor@mittelerde.ch>
2012-02-05 06:20:18 +0100

    gabi urls report upadted

---
+++ reports/gabiUrls.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+function runReport() {
+	$ret = null;
+	$rowCnt = 0;
+	foreach (loadCsv("urls.csv") as $row) {
+		$ret[$rowCnt][0] = $row[0];
+		$ret[$rowCnt][1] = substr($row[1], 0, strrpos($row[1], "."));
+		$ret[$rowCnt][2] = $row[2];
+		$rowCnt++;
+	}
+	return $ret;
+}
+
+
+?>
