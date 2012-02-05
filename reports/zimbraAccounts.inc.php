593c712069909172e30b3e003b044a6d07178e6a
Marc Urben <aegnor@mittelerde.ch>
2012-02-05 05:08:59 +0100

    first report is working now

---
+++ reports/zimbraAccounts.inc.php
@@ -0,0 +1,64 @@
+<?php
+
+function runReport() {
+	$ret = null;
+	$source = loadCsv("accountusage.csv");
+	$archiveDb = "";
+	$accountDb = "";
+	
+	foreach ($source as $row) {
+		$colCnt = 0;
+		list($user, $domain) = split("@", $row[0]);
+		if (strrpos($domain, ".archive") > 0) {
+			$tmpUser = substr($user, 0, strrpos($user, "-"));
+			$tmpDomain = substr($domain, 0, strrpos($domain, ".archive"));
+			$archiveDb[$tmpDomain][$tmpUser][mbarchive] = $row[1];
+		} else {
+			$accountDb[$domain][$user][mbused] = $row[1];
+			$accountDb[$domain][$user][mbquota] = $row[2];
+			$accountDb[$domain][$user][accstatus] = $row[3];
+		}
+	}	
+
+	$rowCnt = 0;
+	foreach ($accountDb as $domain => $domainData) {
+		$domainMbused = 0;
+		$domainMbquota = 0;
+		$domainMbarchive = 0;
+		foreach ($domainData as $user => $userData) {
+			$ret[$rowCnt][0] = $domain;
+			$ret[$rowCnt][1] = $user;
+			$ret[$rowCnt][2] = $userData[mbused];
+			$ret[$rowCnt][3] = $userData[mbquota];
+
+			if ($archiveDb[$domain][$user][mbarchive] == "") {
+				$ret[$rowCnt][4] = "0";
+			} else {
+				$ret[$rowCnt][4] = $archiveDb[$domain][$user][mbarchive];
+			}
+
+			$domainMbused = $domainMbused + $ret[$rowCnt][2];
+			$domainMbquota = $domainMbquota + $ret[$rowCnt][3];
+			$domainMbarchive = $domainMbarchive + $ret[$rowCnt][4];
+			
+			$ret[$rowCnt][5] = $ret[$rowCnt][2] + $ret[$rowCnt][4];
+			$ret[$rowCnt][6] = $userData[accstatus];
+
+			$rowCnt++;
+		}
+
+		$ret[$rowCnt][0] = $domain;
+		$ret[$rowCnt][1] = "&nbsp;";
+		$ret[$rowCnt][2] = $domainMbused;
+		$ret[$rowCnt][3] = $domainMbquota;
+		$ret[$rowCnt][4] = $domainMbarchive;
+		$ret[$rowCnt][5] = $domainMbused + $domainMbarchive;
+		$ret[$rowCnt][6] = "Total";
+
+		$rowCnt++;
+	}
+	return $ret;
+}
+
+
+?>
