6ec1e298c1fc3658feffe41a8bec4aa8c6b99758
Marc Urben <aegnor@mittelerde.ch>
2012-05-23 10:31:40 +0200

    added alert file

---
+++ alert.php
@@ -0,0 +1,17 @@
+<?php
+
+# CSV Output
+
+# Settings
+require_once("inc/settings.inc.php");
+
+# Load saved svc file settings
+require_once("inc/sources.inc.php");
+
+# Load PHP functions
+require_once("inc/functions.inc.php");
+
+
+
+
+?>
