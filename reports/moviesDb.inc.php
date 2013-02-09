6d4ca4cbe69c676fd32d9a3694bb40452ecbe052
Marc Urben <aegnor@mittelerde.ch>
2013-02-09 19:55:38 +0100

    working on moviesdb export csv report

---
+++ reports/moviesDb.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Title"; 0
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Year"; 1
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Country"; 2
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Duration"; 3
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Genere"; 4
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Medium"; 5
+// $GLOBALS["sources"][$filename]["fields"][$cnt++] = "Loaned"; 6
+// loandate 7
+// cover 8
+
+function runReport() {
+	$ret = null;
+	$rowCnt = 0;
+	foreach (loadCsv("urls.csv") as $row) {
+		$ret[$rowCnt][0] = "<img src='" . $row[8] . "' style='max-height: 20px; max-width: 20px'/>" . "<abbr title='" . $row[2] . " " . $row[1] . "'>" . $row[0] . "</abbr>";
+		$ret[$rowCnt][1] = $row[3];
+		$ret[$rowCnt][2] = "<abbr title='" . $row[4] . "'>" . $row[5] . "</abbr>";
+		$ret[$rowCnt][3] = "<abbr title='" . $row[7] . "'>" . $row[6] . "</abbr>";
+		$rowCnt++;
+	}
+	return $ret;
+}
+
+function runReport() {
+	true;
+}
+
+?>
